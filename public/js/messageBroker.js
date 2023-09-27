4f8ddd4d714fa17dbcff22c6b59dde7ae884ddf0
Aleksandr Shatilov <ashatilovdev@gmail.com>
2023-09-27 22:19:15 +0300

    домены

--- public/js/messageBroker.js
+++ public/js/messageBroker.js
@@ -4,7 +4,15 @@
     window.addEventListener(
         "message",
         (event) => {
-         if (event.origin !== "http://localhost:6400" && event.origin !== "http://localhost:4200" && event.origin !== "https://dev.corknote.ru" && event.origin !== "https://corknote.ru" && event.origin !== "https://app.corknote.ru" ) return;
+         if (event.origin !== "http://localhost:6400" 
+          && event.origin !== "http://localhost:4200" 
+          && event.origin !== "https://dev.corknote.ru" 
+          && event.origin !== "https://corknote.ru" 
+          && event.origin !== "https://app.corknote.ru" 
+          && event.origin !== "https://app.probka.tech" 
+          && event.origin !== "https://dev.probka.tech" 
+          ) return;
+          
       
           if (event.data?.action === 'init') {
             const name = document.getElementById('roomName');
